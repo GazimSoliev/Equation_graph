4e27ad2862ab261ddfcfca775084924a172ac8e5
Gazim <gazim1304@gmail.com>
2023-02-22 10:55:32 +0500

    Add markups

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -4,7 +4,7 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.onDrag
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ZoomIn
@@ -18,6 +18,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
@@ -31,13 +32,14 @@ fun App() {
     MaterialTheme {
         Surface {
             Column {
+                val pixelStep = 1f
                 EquationGraph(
                     modifier = Modifier.fillMaxSize(),
                     equations = listOf(
-                        EquationGraphLine(equation = { it }, lineColor = Color.Blue, pixelStep = 1f),
-                        EquationGraphLine(equation = { it * it }, lineColor = Color.Red, pixelStep = 1f),
-                        EquationGraphLine(equation = { sin(it) }, lineColor = Color.Cyan, pixelStep = 1f),
-                        EquationGraphLine(equation = { cos(it) }, lineColor = Color.Green, pixelStep = 1f)
+                        EquationGraphLine(equation = { it }, lineColor = Color.Blue, pixelStep = pixelStep),
+                        EquationGraphLine(equation = { it * it }, lineColor = Color.Red, pixelStep = pixelStep),
+                        EquationGraphLine(equation = { sin(it) }, lineColor = Color.Cyan, pixelStep = pixelStep),
+                        EquationGraphLine(equation = { cos(it) }, lineColor = Color.Green, pixelStep = pixelStep)
                     )
                 )
             }
@@ -74,7 +76,13 @@ fun EquationGraph(
     Column {
         EquationGraphCanvas(
             modifier = Modifier.fillMaxWidth().weight(1f)
-                .onDrag { relativeOffset = Offset(relativeOffset.x + it.x, relativeOffset.y + it.y) },
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        relativeOffset =
+                            Offset(relativeOffset.x + dragAmount.x, relativeOffset.y + dragAmount.y)
+                    }
+                },
             equations = equations,
             oneUnitToPixels = unitToPixels,
             relativeOffset = relativeOffset
@@ -128,22 +136,11 @@ fun EquationGraphCanvas(
 ) = Canvas(
     modifier = modifier
 ) {
-    val centerX = size.width * ratioX
-    val centerY = size.height * ratioY
-    drawLine(
-        start = Offset(centerX + relativeOffset.x, 0f),
-        end = Offset(centerX + relativeOffset.x, size.height),
-        color = Color.Black,
-        strokeWidth = 1f,
-        alpha = 0.5f
-    )
-    drawLine(
-        start = Offset(0f, centerY + relativeOffset.y),
-        end = Offset(size.width, centerY + relativeOffset.y),
-        color = Color.Black,
-        strokeWidth = 1f,
-        alpha = 0.5f
-    )
+    val height = size.height
+    val width = size.width
+    val centerY = height * ratioY
+    val centerX = width * ratioX
+    drawEquationGrid(centerX + relativeOffset.x, centerY + relativeOffset.y)
     equations.forEach {
         drawEquationLine(
             equation = it.equation,
@@ -158,6 +155,85 @@ fun EquationGraphCanvas(
             relativeOffsetY = relativeOffset.y + centerY
         )
     }
+}
+
+fun DrawScope.drawEquationGrid(
+    relativeX: Float, relativeY: Float
+) {
+    fun doSome(relative: Float, length: Float, markupStep: Float): MutableList<Float> {
+        val markups = mutableListOf<Float>()
+        val relativeGrid = relative % markupStep
+        var currentMarkupX = relativeGrid
+        while (currentMarkupX > 0f) {
+            markups.add(currentMarkupX)
+            currentMarkupX -= markupStep
+        }
+        currentMarkupX = relativeGrid
+        while (currentMarkupX < length) {
+            markups.add(currentMarkupX)
+            currentMarkupX += markupStep
+        }
+        markups.remove(relative)
+        println(markups)
+        return markups
+    }
+
+    val smallMarkupsX = doSome(relativeX, size.width, 25f)
+    val smallMarkupsY = doSome(relativeY, size.height, 25f)
+    val markupsX = doSome(relativeX, size.width, 100f)
+    val markupsY = doSome(relativeY, size.height, 100f)
+    smallMarkupsX.forEach {
+        drawLine(
+            start = Offset(it, 0f),
+            end = Offset(it, size.height),
+            color = Color.Black,
+            strokeWidth = 1f,
+            alpha = 0.05f
+        )
+    }
+    smallMarkupsY.forEach {
+        drawLine(
+            start = Offset(0f, it),
+            end = Offset(size.width, it),
+            color = Color.Black,
+            strokeWidth = 1f,
+            alpha = 0.05f
+        )
+    }
+    markupsX.forEach {
+        drawLine(
+            start = Offset(it, 0f),
+            end = Offset(it, size.height),
+            color = Color.Black,
+            strokeWidth = 1f,
+            alpha = 0.1f
+        )
+    }
+    markupsY.forEach {
+        drawLine(
+            start = Offset(0f, it),
+            end = Offset(size.width, it),
+            color = Color.Black,
+            strokeWidth = 1f,
+            alpha = 0.1f
+        )
+    }
+    if (relativeX in 0f..size.width)
+        drawLine(
+            start = Offset(relativeX, 0f),
+            end = Offset(relativeX, size.height),
+            color = Color.Black,
+            strokeWidth = 1f,
+            alpha = 1f
+        )
+    if (relativeY in 0f..size.height)
+        drawLine(
+            start = Offset(0f, relativeY),
+            end = Offset(size.width, relativeY),
+            color = Color.Black,
+            strokeWidth = 1f,
+            alpha = 1f
+        )
 }
 
 
